6159c5be0023ac8922c0055d55e698d362e4a891
SirL0fty <sirl0fty@gmail.com>
2023-03-26 20:14:57 +0100

    Materhead and League section created including navbar built into the layout.html

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/')
+def index():
+        return render_template('index.html')
+
+
+app.run(debug=True, port=8000, host='0.0.0.0')
