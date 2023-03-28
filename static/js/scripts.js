59eb879dad490eb7de0089f02daecfa5d3c3914b
SirL0fty <sirl0fty@gmail.com>
2023-03-28 21:06:17 +0100

    Updated the feature section, contact form, and map

---
+++ static/js/scripts.js
@@ -0,0 +1,31 @@
+const navEl = document.querySelector('.navbar');
+
+        window.addEventListener('scroll', () => {
+                if (window.scrollY >= 70) {
+                        navEl.classList.add('navbar-scrolled')
+                } else if (window.scrollY < 70) {
+                        navEl.classList.remove('navbar-scrolled');
+                }
+        })
+
+
+function submitForm() {
+  // Get form data
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var message = document.getElementById("message").value;
+
+  // Send form data to server (you would need to implement this part yourself)
+  // ...
+
+  // Show success message
+        $("#successModal").modal("show");
+
+  // Clear the form
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+
+  // Prevent the form from submitting normally
+        return false;
+}
