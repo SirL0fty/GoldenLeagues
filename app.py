f727ec128544b2e8020660217e71b93929a9653b
SirL0fty <sirl0fty@gmail.com>
2023-04-02 00:51:14 +0100

    added dunders

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ app = Flask(__name__)
 def index():
         return render_template('index.html', footer='footer.html')
 
-
-app.run(debug=True, port=5050, host='0.0.0.0')
+if __name__ == "__main__"
+        app.run(debug=True, port=8000, host='0.0.0.0')
