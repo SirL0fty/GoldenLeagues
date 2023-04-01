9e620a8b71a5098c8df1d3c6256204603ed597d8
SirL0fty <sirl0fty@gmail.com>
2023-04-02 00:51:50 +0100

    bug fix

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ app = Flask(__name__)
 def index():
         return render_template('index.html', footer='footer.html')
 
-if __name__ == "__main__"
+if __name__ == "__main__"\;
         app.run(debug=True, port=8000, host='0.0.0.0')
