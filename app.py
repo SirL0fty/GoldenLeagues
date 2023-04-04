2e7894ed189583c70fc5acde8c00eb6ae1849603
SirL0fty <sirl0fty@gmail.com>
2023-04-04 15:26:07 +0100

    fixed app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
+
 def run_app():
         app = Flask(__name__)
-        app.run(debug=True, port=5050, host='0.0.0.0')
 
         @app.route('/', methods=['GET', 'POST'])
         def index():
                 return render_template('index.html', footer='footer.html')
+
+        app.run(debug=True, port=5050, host='0.0.0.0')
 
         return app
