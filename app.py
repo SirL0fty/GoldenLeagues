ff292a22221f03d8bcf7bceef521c46ee9990cb8
SirL0fty <sirl0fty@gmail.com>
2023-04-12 20:15:37 +0100

    Tweaking styles and app.py run

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
 
+app = Flask(__name__)
 
-def run_app():
-        app = Flask(__name__)
 
-        @app.route('/', methods=['GET', 'POST'])
-        def index():
-                return render_template('index.html', footer='footer.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+        return render_template('index.html', footer='footer.html')
 
+
+if __name__ == '__main__':
         app.run(debug=True, port=5050, host='0.0.0.0')
-
-        return app
