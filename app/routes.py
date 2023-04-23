6f65c41225b69760ff8bc105552e776a62c52b6b
SirL0fty <sirl0fty@gmail.com>
2023-04-23 21:27:19 +0100

    form changes

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,22 @@
 from flask import render_template, request
 from app import app 
-from app.forms import ContactForm
+# from app.forms import ContactForm
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = ContactForm()
+    # form = ContactForm()
 
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
-        message = form.message.data
+    # if request.method == 'POST' and form.validate():
+    #     name = form.name.data
+    #     email = form.email.data
+    #     message = form.message.data
 
-        # Do something with the data
+    #     # Do something with the data
 
-        return render_template('index.html', form=form)
+    #     return render_template('index.html', form=form)
 
-    return render_template('index.html', form=form)
+    # return render_template('index.html', form=form)
+    return render_template('index.html')
 
 
 
