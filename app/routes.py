d552c1851122360f5df66b3977eab0833c3b0c21
SirL0fty <sirl0fty@gmail.com>
2023-04-23 21:24:04 +0100

    removed form.py for the time being

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,19 @@ from app.forms import ContactForm
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-        return render_template('index.html')
+    form = ContactForm()
+
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+
+        # Do something with the data
+
+        return render_template('index.html', form=form)
+
+    return render_template('index.html', form=form)
+
 
 
 @app.route('/tennis')
--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,27 +1,30 @@
+window.onload = function () {
+  initMap();
+};
+
 function initMap() {
+  var myLatLng = { lat: 50.877196177898774, lng: -1.3280979847009355 };
 
-        var myLatLng = { lat: 50.877196177898774, lng: -1.3280979847009355 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: myLatLng,
+  });
 
-        var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: myLatLng,
-})
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Our Location',
+  });
 
-        var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: "Our Location",
-});
+  var infowindow = new google.maps.InfoWindow({
+    content: 'Our Sports Complex, Satchell Ln, Hamble-le-Rice, Southampton SO31 4NE',
+  });
 
-        var infowindow = new google.maps.InfoWindow({
-        content: "Our Sports Complex, Satchell Ln, Hamble-le-Rice, Southampton SO31 4NE",
-        });
+  marker.addListener('mouseover', function () {
+    infowindow.open(map, marker);
+  });
 
-        marker.addListener("mouseover", function () {
-        infowindow.open(map, marker);
-});
-
-        marker.addListener("mouseout", function () {
-        infowindow.close();
-});
+  marker.addListener('mouseout', function () {
+    infowindow.close();
+  });
 }
