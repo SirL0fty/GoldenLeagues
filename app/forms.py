85355eacfba4c46dcc17302f1decc397d055977f
SirL0fty <sirl0fty@gmail.com>
2023-04-23 21:25:05 +0100

    formed edit

--- app/forms.py
+++ app/forms.py
@@ -2,18 +2,8 @@
 # from wtforms import StringField, SubmitField
 # from wtforms.validators import DataRequired, Email, Length
 
-
-<<<<<<< HEAD
 # class ContactForm(FlaskForm):
 #     name = StringField('Name', validators=[DataRequired(), Length(max=255)])
 #     email = StringField('Email', validators=[DataRequired(), Length(max=255), Email()])
 #     message = StringField('Message',validators=[DataRequired(), Length(max=255)])
 #     submitForm = SubmitField('Submit')
-=======
-class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(max=255)])
-    email = StringField('Email', validators=[DataRequired(), Length(max=255), Email()])
-    message = StringField('Message',validators=[DataRequired(), Length(max=255)])
-    submitForm = SubmitField('Submit')
-
->>>>>>> 33638da92dffee8b4e12265a468d238001884063
