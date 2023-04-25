b903d2fe3ecc49ba7eed1956ecde9ec5f18e8413
SirL0fty <sirl0fty@gmail.com>
2023-04-25 22:27:43 +0100

    Revert "cleaned code"

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 
+<<<<<<< HEAD
 from app.database import db
 from app.models import User, League
 
+=======
+>>>>>>> parent of 56d3a0f (cleaned code)
 # class ContactForm(FlaskForm):
 #     name = StringField('Name', validators=[DataRequired(), Length(max=255)])
 #     email = StringField('Email', validators=[DataRequired(), Length(max=255), Email()])
@@ -19,7 +22,14 @@ class UserForm(FlaskForm):
         submit = SubmitField('Add User')
         
         
+<<<<<<< HEAD
 class LeagueForm(FlaskForm):
         name = StringField('Name', validators=[DataRequired(), Length(max=255)])
         owner_id = StringField('Owner', validators=[DataRequired(), Length(max=255)])
         submit = SubmitField('Add League')
+=======
+# class LeagueForm(FlaskForm):
+#         name = StringField('Name', validators=[DataRequired(), Length(max=255)])
+#         owner_id = StringField('Owner', validators=[DataRequired(), Length(max=255)])
+#         submit = SubmitField('Add League')
+>>>>>>> parent of 56d3a0f (cleaned code)
