673ab537fd6f680dda11e703e3699dbe0f0c64fb
SirL0fty <sirl0fty@gmail.com>
2023-04-25 22:05:25 +0100

    Updated Import Cirulars

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from app.database import db
+from app import db
 
 
 class User(db.Model):
@@ -10,14 +10,5 @@ class User(db.Model):
         created_at = db.Column(db.DateTime, server_default=db.func.now())
         updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
         
-        league = db.relationship('League', back_populates='owner')
+        # league = db.relationship('League', back_populates='owner')
         
-
-class League(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(255))
-        owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-        created_at = db.Column(db.DateTime, server_default=db.func.now())
-        updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-
-        user = db.relationship('User', back_populates='league')
