e816278a4a6deeff4bd0384b19360d07d9c315bf
SirL0fty <sirl0fty@gmail.com>
2023-04-25 22:21:04 +0100

    Revert "Update Import Circulars"
    This reverts commit 605a9c6008ed0a7324438123d734a5efff36ba9d.

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python
-import os
-
 from flask import Flask
-from dotenv import load_dotenv
-
-from app.database import db
-from app.models import *
-
-load_dotenv()
 
 app = Flask(__name__)
-
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST', '127.0.0.1')
-db_port = os.getenv('DB_PORT', '3306')
-db_name = os.getenv('DB_NAME')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
-
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-db.init_app(app)
+app.config['SECRET_KEY'] = 'sQ9kocDCG2kMAfrjsHAmBpxui3XkEa6ZQReTiqDd'
 
 from app import routes 
+
+
+# from dotenv import load_dotnev 
+
+# load_dotnev()
+# from email_validator import validate_email, EmailNotValidError
+# def is_valid_email(email):
+#     try:
+#         valid = validate_email(email)
+#         email = valid.email
+#         except EmailNotValidError as e:
+#         return False
+#         return True
+# if is_valid_email('test@example.com'):
+#     print('Valid email!')
+# else:
+#     print('Invalid email.')
+
+
