218375425b9f73a721f6285bc9e33aef62e0d941
SirL0fty <sirl0fty@gmail.com>
2023-04-25 22:21:04 +0100

    Revert "Update Import Circulars"
    This reverts commit 605a9c6008ed0a7324438123d734a5efff36ba9d.

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 
+from app.database import db
+from app.models import User, League
+
 # class ContactForm(FlaskForm):
 #     name = StringField('Name', validators=[DataRequired(), Length(max=255)])
 #     email = StringField('Email', validators=[DataRequired(), Length(max=255), Email()])
@@ -16,7 +19,7 @@ class UserForm(FlaskForm):
         submit = SubmitField('Add User')
         
         
-# class LeagueForm(FlaskForm):
-#         name = StringField('Name', validators=[DataRequired(), Length(max=255)])
-#         owner_id = StringField('Owner', validators=[DataRequired(), Length(max=255)])
-#         submit = SubmitField('Add League')
+class LeagueForm(FlaskForm):
+        name = StringField('Name', validators=[DataRequired(), Length(max=255)])
+        owner_id = StringField('Owner', validators=[DataRequired(), Length(max=255)])
+        submit = SubmitField('Add League')
