2732931885ae58e31bdadcd34f601193d6c56b70
SirL0fty <sirl0fty@gmail.com>
2023-04-25 22:30:51 +0100

    Revert "Updated Import Cirulars"
    This reverts commit 673ab537fd6f680dda11e703e3699dbe0f0c64fb.

--- seeder.py
+++
@@ -1,7 +0,0 @@
-from app import app, db, User
-
-USERS = [
-                {name: 'Mike', email: 'mikecrane@me.com', password: 'password123'},
-                {name: 'Sam', email: 'samuelaaronparker@gmail.com', password: 'password123'},
-                {name: 'Kat', email: 'katrinadematos3@gmail.com', password: 'password123'},
-]
