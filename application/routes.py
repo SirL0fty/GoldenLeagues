827bce77fbc23e2fdf80e62e53f7f1464a113ca0
SirL0fty <sirl0fty@gmail.com>
2023-04-26 20:39:25 +0100

    updated route

--- application/routes.py
+++ application/routes.py
@@ -3,10 +3,10 @@ from flask import flash, render_template, request, redirect, abort, session
 from application import app, db
 from application.models import User
 from werkzeug.datastructures import MultiDict
-from application.forms import UserForm
+from application.forms import RegisterForm
 from sqlalchemy.sql.functions import now
 
-USER_FORM_DATA = "user_form"
+REGISTER_FORM_DATA = "register_form"
 LOGGED_IN_USER = 'user_id'
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,28 +16,28 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    user_form = UserForm()
+    register_form = RegisterForm()
 
-    if request.method == 'POST' and user_form.validate():
-        user = User.query.filter_by(email=user_form.email.data).first()
+    if request.method == 'POST' and register_form.validate():
+        user = User.query.filter_by(email=register_form.email.data).first()
         if user:
             flash(f"{user.email } - Email already exists. Please log in.")
             return redirect('/login')
         
         new_user = User(
-            name=user_form.name.data,
-            email=user_form.email.data,
-            password=user_form.password.data
+            name=register_form.name.data,
+            email=register_form.email.data,
+            password=register_form.password.data
         )
 
         db.session.add(new_user)
         db.session.commit()
         
         flash(f"{new_user.name} successfully registered.")
-        session[USER_FORM_DATA] = request.form
+        session[REGISTER_FORM_DATA] = request.form
         return redirect('/user')
 
-    return render_template('register.html', form=user_form)
+    return render_template('register.html', form=register_form)
 
 
 @app.route('/tennis')
