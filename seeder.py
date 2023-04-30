b5a7dc2e57d1d55346f07ae9ab5f96b072d19b36
katydee113 <124299643+katydee113@users.noreply.github.com>
2023-04-30 23:11:52 +0100

    team rules acordian fixed

--- seeder.py
+++ seeder.py
@@ -4,29 +4,14 @@ from application import db, app
 from application.models import User
 
 USERS = [
-    User(
-        name="Mike",
-        email="mikecrane@me.com",
-        password="password123",
-        address="Address1, Address2, City, County, PO167JU",
-        phone="",
-        club="Fareham Tennis Club",
-    ),
-    User(
-        name="Sam",
-        email="samuelaaronparker@gmail.com",
-        password="password123",
-        address="",
-        phone="",
-        club="",
-    ),
+    
     User(
         name="Kat",
         email="katrinadematos3@gmail.com",
-        password="password123",
-        address="",
-        phone="",
-        club="",
+        _password="CoolCat",
+        address="26 Starbuck Road",
+        phone="07801506457",
+        club="CoolDaddy",
     ),
 ]
 
