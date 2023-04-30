1149e1df8443cc9b03e4d4bb83342922b4426714
SirL0fty <sirl0fty@gmail.com>
2023-04-30 21:07:52 +0100

    Fixed Registration form

--- seeder.py
+++ seeder.py
@@ -4,13 +4,34 @@ from application import db, app
 from application.models import User
 
 USERS = [
-    User(name="Mike", email="mikecrane@me.com", password="password123"),
-    User(name="Sam", email="samuelaaronparker@gmail.com", password="password123"),
-    User(name="Kat", email="katrinadematos3@gmail.com", password="password123"),
+    User(
+        name="Mike",
+        email="mikecrane@me.com",
+        password="password123",
+        address="Address1, Address2, City, County, PO167JU",
+        phone="",
+        club="Fareham Tennis Club",
+    ),
+    User(
+        name="Sam",
+        email="samuelaaronparker@gmail.com",
+        password="password123",
+        address="",
+        phone="",
+        club="",
+    ),
+    User(
+        name="Kat",
+        email="katrinadematos3@gmail.com",
+        password="password123",
+        address="",
+        phone="",
+        club="",
+    ),
 ]
 
 with app.app_context():
     db.session.add_all(USERS)
-    db.session.commit()
+db.session.commit()
 
 print("Database seeded successfully!")
